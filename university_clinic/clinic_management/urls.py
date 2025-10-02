538ba3a258f168a8e33322a2c32a1b31a2bcfc7d
Ahmad Muh'd-Danrangi <159925950+Danrangi@users.noreply.github.com>
2025-10-02 10:54:49 +0000

    update

--- university_clinic/clinic_management/urls.py
+++ university_clinic/clinic_management/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+def redirect_to_login(request):
+    return redirect('login')
 
 urlpatterns = [
+    path('', redirect_to_login, name='home'),  # Add this line for root URL
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
